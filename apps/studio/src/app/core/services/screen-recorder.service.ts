9a68b9a92cbed5234707e61f24a91076c320ae26
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2023-10-28 22:02:10 +0200

    feat(studio): add screen recorder service

---
+++ apps/studio/src/app/core/services/screen-recorder.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { defer, Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ScreenRecorderService {
+  video!: HTMLVideoElement;
+
+  public getStream(): Observable<MediaStream> {
+    return defer(() => this.getDisplayMedia()).pipe(
+      tap((stream) => this.initStream(stream))
+    );
+  }
+
+  private getDisplayMedia(): Promise<MediaStream> {
+    return navigator.mediaDevices.getDisplayMedia({
+      video: {
+        width: 1280,
+        height: 720,
+        displaySurface: 'default',
+      },
+    });
+  }
+
+  private initStream(stream: MediaStream): void {
+    window.stream = stream;
+    this.video.srcObject = stream;
+  }
+}
