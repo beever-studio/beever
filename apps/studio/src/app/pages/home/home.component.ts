a21727e8de2fbb56790e520d464a65b176bd0a64
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2023-10-28 18:51:44 +0200

    feat(studio): add home component/route

---
+++ apps/studio/src/app/pages/home/home.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'beever-home',
+  standalone: true,
+  template: `
+    <div class="flex flex-col sm:flex-row items-center justify-center gap-8">
+      <div class="flex flex-col items-center">
+        <h1 class="font-caveat text-8xl text-white mb-4">BEEVER</h1>
+        <p class="text-3xl text-white text-center">
+          Your open source
+          <span class="whitespace-nowrap">streaming studio</span>
+        </p>
+      </div>
+      <img class="w-80 h-auto" src="assets/images/logo.png" alt="" />
+    </div>
+    <div class="flex justify-center gap-4 py-8">
+      <a
+        mat-stroked-button
+        color="accent"
+        routerLink="/record"
+        [queryParams]="{ source: 'camera' }"
+        >Camera recorder</a
+      >
+      <a
+        mat-stroked-button
+        color="accent"
+        routerLink="/record"
+        [queryParams]="{ source: 'audio' }"
+        >Mic recorder</a
+      >
+      <a
+        mat-stroked-button
+        color="accent"
+        routerLink="/record"
+        [queryParams]="{ source: 'screen' }"
+        >Screen recorder</a
+      >
+    </div>
+  `,
+  imports: [RouterLink, MatButtonModule],
+})
+export default class HomeComponent {}
