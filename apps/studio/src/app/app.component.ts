dfb9df974ab8ef9293f18889613c8b4de758e387
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2023-10-28 17:48:41 +0200

    feat(studio): register svg icons

--- apps/studio/src/app/app.component.ts
+++ apps/studio/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   standalone: true,
@@ -8,4 +10,27 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'volume_off',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/volume_off.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'volume',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/volume.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'videocam',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/videocam.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'videocam_off',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/videocam_off.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'capture',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/capture.svg')
+    );
+  }
+}
