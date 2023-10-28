37f1bf86bce4f29f750b9e69e657f964cf0278cd
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2023-10-28 22:01:04 +0200

    feat(studio): add video controls component

--- apps/studio/src/app/pages/record/record.component.ts
+++ apps/studio/src/app/pages/record/record.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { VideoComponent } from '../../core/components/video.component';
+import { VideoControlsComponent } from '../../core/components/video-controls.component';
 
 @Component({
   selector: 'beever-record',
   standalone: true,
-  template: ` <beever-video></beever-video> `,
-  imports: [VideoComponent],
+  template: `
+    <beever-video></beever-video>
+    <beever-video-controls></beever-video-controls>
+  `,
+  imports: [VideoComponent, VideoControlsComponent],
 })
 export default class RecordComponent {}
