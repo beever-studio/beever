24fd568fc49d6dfc6296fe743ab5af8a5a6e3930
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2023-10-28 18:46:07 +0200

    feat(studio): add header component

---
+++ apps/studio/src/app/core/components/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'beever-header',
+  standalone: true,
+  template: `
+    <header class="flex justify-between">
+      <a routerLink="/" class="font-caveat text-4xl text-white"> BEEVER </a>
+    </header>
+  `,
+  imports: [RouterLink],
+})
+export class HeaderComponent {}
