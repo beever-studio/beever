d0e2821c38a4005178b1d3807160751874c94c00
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2023-12-15 19:56:31 +0100

    fix(studio): update types

--- apps/studio/src/app/core/models/format.model.ts
+++ apps/studio/src/app/core/models/format.model.ts
@@ -1,14 +1,19 @@
+export type FormatType = 'landscape' | 'portrait';
+
 export interface Format {
+  type: FormatType;
   width: number;
   height: number;
 }
 
 export const LANDSCAPE_FORMAT: Format = {
+  type: 'landscape',
   width: 1280,
   height: 720,
 };
 
 export const PORTRAIT_FORMAT: Format = {
+  type: 'portrait',
   width: 0,
   height: 0,
 };
